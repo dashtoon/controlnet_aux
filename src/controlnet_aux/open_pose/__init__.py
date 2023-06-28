016c1fe1f2acb754be267e32d7f665411723800e
Ayushman Buragohain <aburagohain160@gmail.com>
2023-06-28 12:56:54 +0530

    break out arguments for hand and face (#1)
    * break out arguments for hand and face
    
    * gracefully handle previous arguments to allow backward compatibility
    
    * remove kwargs

--- src/controlnet_aux/open_pose/__init__.py
+++ src/controlnet_aux/open_pose/__init__.py
@@ -1,6 +1,8 @@
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
+import warnings
+
 import torch
 import cv2
 import numpy as np
@@ -75,7 +77,7 @@ class OpenposeDetector:
 
         return cls(body_estimation, hand_estimation, face_estimation)
 
-    def __call__(self, input_image, detect_resolution=512, image_resolution=512, hand_and_face=False, return_pil=True):
+    def __call__(self, input_image, detect_resolution=512, image_resolution=512, hand_and_face=False, hand=False, face=False, return_pil=True):
         # hand = False
         if not isinstance(input_image, np.ndarray):
             input_image = np.array(input_image, dtype=np.uint8)
@@ -88,7 +90,13 @@ class OpenposeDetector:
             candidate, subset = self.body_estimation(input_image)
             hands = []
             faces = []
+
             if hand_and_face:
+                warnings.warn("`hand_and_face` argument is deprecated. Use `hand` and `face` arguments instead.", DeprecationWarning)
+                hand = hand_and_face
+                face = hand_and_face
+
+            if hand:
                 # Hand
                 hands_list = handDetect(candidate, subset, input_image)
                 for x, y, w, is_left in hands_list:
@@ -97,7 +105,7 @@ class OpenposeDetector:
                         peaks[:, 0] = np.where(peaks[:, 0] < 1e-6, -1, peaks[:, 0] + x) / float(W)
                         peaks[:, 1] = np.where(peaks[:, 1] < 1e-6, -1, peaks[:, 1] + y) / float(H)
                         hands.append(peaks.tolist())
-                # Face
+            if face:
                 faces_list = faceDetect(candidate, subset, input_image)
                 for x, y, w in faces_list:
                     heatmaps = self.face_estimation(input_image[y:y+w, x:x+w, :])
